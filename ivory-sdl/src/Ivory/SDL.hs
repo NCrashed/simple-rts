8643a9dfcd9a9e262fe8701f26004dc675b3c870
Anton Gushcha <ncrashed@gmail.com>
2020-06-09 04:28:47 +0000

    Add sdlCreateRenderer

--- ivory-sdl/src/Ivory/SDL.hs
+++ ivory-sdl/src/Ivory/SDL.hs
@@ -3,6 +3,7 @@ module Ivory.SDL(
   -- * Initialization
   , sdlInit
   -- ** Init flags
+  , InitFlag
   , sdlInitTimer
   , sdlInitAudio
   , sdlInitVideo
@@ -14,12 +15,13 @@ module Ivory.SDL(
   -- * Logging
   , SdlLog(..)
   , sdlGetError
-  -- * Window creation
+  -- * Window
   , SDLWindow
   , sdlCreateWindow
   , sdlWindowPosCentered
   , sdlWindowPosUndefined
   -- ** Window flags
+  , WindowFlag
   , sdlWindowFullscreen
   , sdlWindowFullscreenDesktop
   , sdlWindowOpenGL
@@ -31,13 +33,24 @@ module Ivory.SDL(
   , sdlWindowMaximized
   , sdlWindowInputGrabbed
   , sdlWindowAllowHighDpi
+  -- * Renderer
+  , SDLRenderer
+  , sdlCreateRenderer
+  -- ** Renderer flags
+  , RenderFlag
+  , sdlRendererSoftware
+  , sdlRendererAccelerated
+  , sdlRendererPresentVSync
+  , sdlRendererTargetTexture
   ) where
 
 import Ivory.Language
 import Ivory.Language.Pointer
 
+type InitFlag = Uint32
+
 sdlInitTimer, sdlInitAudio, sdlInitVideo, sdlInitJoystick, sdlInitHaptic,
-  sdlInitGameController, sdlInitEvents, sdlInitEverything :: Uint32
+  sdlInitGameController, sdlInitEvents, sdlInitEverything :: InitFlag
 sdlInitTimer          = extern "SDL_INIT_TIMER" "SDL.h"
 sdlInitAudio          = extern "SDL_INIT_AUDIO" "SDL.h"
 sdlInitVideo          = extern "SDL_INIT_VIDEO" "SDL.h"
@@ -47,7 +60,7 @@ sdlInitGameController = extern "SDL_INIT_GAMECONTROLLER" "SDL.h"
 sdlInitEvents         = extern "SDL_INIT_EVENTS" "SDL.h"
 sdlInitEverything     = extern "SDL_INIT_EVERYTHING" "SDL.h"
 
-sdlInit :: Def ('[Uint32] :-> IBool)
+sdlInit :: Def ('[InitFlag] :-> IBool)
 sdlInit = importProc "SDL_Init" "SDL.h"
 
 class SdlLog a where
@@ -82,7 +95,9 @@ sdlGetError = importProc "SDL_GetError" "SDL.h"
 
 type SDLWindow = Pointer Nullable Mutable Global (Stored OpaqueType)
 
-sdlCreateWindow :: Def ('[IString, Sint32, Sint32, Sint32, Sint32, Uint32] :-> SDLWindow)
+type WindowFlag = Uint32
+
+sdlCreateWindow :: Def ('[IString, Sint32, Sint32, Sint32, Sint32, WindowFlag] :-> SDLWindow)
 sdlCreateWindow = importProc "SDL_CreateWindow" "SDL.h"
 
 sdlWindowPosCentered :: Sint32
@@ -93,7 +108,7 @@ sdlWindowPosUndefined = extern "SDL_WINDOWPOS_UNDEFINED" "SDL.h"
 
 sdlWindowFullscreen, sdlWindowFullscreenDesktop, sdlWindowOpenGL, sdlWindowVulkan,
   sdlWindowHidden, sdlWindowBorderless, sdlWindowResizable, sdlWindowMinimized,
-  sdlWindowMaximized, sdlWindowInputGrabbed, sdlWindowAllowHighDpi :: Sint32
+  sdlWindowMaximized, sdlWindowInputGrabbed, sdlWindowAllowHighDpi :: WindowFlag
 sdlWindowFullscreen         = extern "SDL_WINDOW_FULLSCREEN" "SDL.h"
 sdlWindowFullscreenDesktop  = extern "SDL_WINDOW_FULLSCREEN_DESKTOP" "SDL.h"
 sdlWindowOpenGL             = extern "SDL_WINDOW_OPENGL" "SDL.h"
@@ -106,9 +121,23 @@ sdlWindowMaximized          = extern "SDL_WINDOW_MAXIMIZED" "SDL.h"
 sdlWindowInputGrabbed       = extern "SDL_WINDOW_INPUT_GRABBED" "SDL.h"
 sdlWindowAllowHighDpi       = extern "SDL_WINDOW_ALLOW_HIGHDPI" "SDL.h"
 
+type SDLRenderer = Pointer Nullable Mutable Global (Stored OpaqueType)
+
+type RenderFlag = Uint32
+
+sdlRendererSoftware, sdlRendererAccelerated, sdlRendererPresentVSync, sdlRendererTargetTexture :: RenderFlag
+sdlRendererSoftware = extern "SDL_RENDERER_SOFTWARE" "SDL.h"
+sdlRendererAccelerated = extern "SDL_RENDERER_ACCELERATED" "SDL.h"
+sdlRendererPresentVSync = extern "SDL_RENDERER_PRESENTVSYNC" "SDL.h"
+sdlRendererTargetTexture = extern "SDL_RENDERER_TARGETTEXTURE" "SDL.h"
+
+sdlCreateRenderer :: Def ('[SDLWindow, Sint32, RenderFlag] :-> SDLRenderer)
+sdlCreateRenderer = importProc "SDL_CreateRenderer" "SDL.h"
+
 sdlModule :: Module
 sdlModule = package "ivory_sdl" $ do
   incl sdlInit
   incl (sdlLog :: Def ('[IString] :-> ()))
   incl sdlGetError
   incl sdlCreateWindow
+  incl sdlCreateRenderer
